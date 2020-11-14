2e10adb8c711197acd1b6225f837f008cab3ab43
Vianney Kabamba <vianneyk@hotmail.co.uk>
2020-11-14 21:44:48 +0000

    script variables

---
+++ script.js
@@ -0,0 +1,15 @@
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+var questionsEl = document.getElementById("questions");
+var quizTimer = document.getElementById("timer");
+var startQuizButton = document.getElementById("startbtn");
+var startQuizDiv = document.getElementById("startpage");
+var quizBody = document.getElementById("quiz");
+var resultsEl = document.getElementById("result");
+var finalScoreEl = document.getElementById("finalScore");
+var gameoverDiv = document.getElementById("gameover");
+var submitScoreBtn = document.getElementById("submitScore");
+var endGameBtns = document.getElementById("endGameBtns");
+
